f71a29b4c36356be5ae16ba8ec4ea624ad4419b7
CanmingCai <a01770368@tec.mx>
2024-04-28 15:02:31 -0600

    nice

--- generar_genes.py
+++ generar_genes.py
@@ -1,6 +1,7 @@
 import csv
 import random
 
+# leer periodos de UDF.csv
 def leer_periodos():
     periodos = {}
     with open('UDF.csv', newline='', encoding='utf-8-sig') as csvfile:
@@ -9,22 +10,36 @@ def leer_periodos():
             periodos[row['Clave']] = [row['P1'], row['P2'], row['P3']]
     return periodos
 
+# leer profesores y materias de Profesores y Materias.csv
+def leer_profesores_materias():
+    profesores_materias = {}
+    with open('Profesores y Materias.csv', newline='', encoding='utf-8-sig') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            #Victor Manion,Juan Alvarado,Roberto Leyva,Mauricio Paletta,Yerly Flores,Jaime Lopez,Jorge Rodriguez,Jose Aguilera,Luis Guadarrama,Pedro Hernandez,Maria Mirafuentes,Roberto Vera,Octavio Silva,Fernando Ruiz,Ivan Olmos,Israel Tabarez
+            profesores_materias[row['Clave']] = [row['Victor Manion'], row['Juan Alvarado'], row['Roberto Leyva'], row['Mauricio Paletta'], row['Yerly Flores'], row['Jaime Lopez'], row['Jorge Rodriguez'], row['Jose Aguilera'], row['Luis Guadarrama'], row['Pedro Hernandez'], row['Maria Mirafuentes'], row['Roberto Vera'], row['Octavio Silva'], row['Fernando Ruiz'], row['Ivan Olmos'], row['Israel Tabarez']]
+    return profesores_materias
+
 def generar_cromosomas():
     cromosoma = []
 
     periodos = leer_periodos()
+    profesores_materias = leer_profesores_materias()
 
-    # 读取CSV文件并获取课程及其班级信息
+    # leer CSV
     cursos = []
     with open('Febrero-Junio.csv', newline='', encoding='utf-8-sig') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
             cursos.append({'CLAVE': row['CLAVE'], '# GPOs': int(row['# GPOs'])})
 
-    # 遍历课程，生成对应数量的班级
+    # generar cantidad de grupos por curso
     for curso in cursos:
+
+        # generar grupos
         for i in range(curso['# GPOs']):
             grupo = i + 1
+            
             # periodos
             periodos_curso = periodos[curso['CLAVE']]
             print(periodos_curso)
@@ -33,17 +48,29 @@ def generar_cromosomas():
                 if periodo == 'X':
                     periodos_disponibles.append(f'P{j+1}')
             periodo = ','.join(periodos_disponibles) if periodos_disponibles else 'N/A'
-            cromosoma.append({'Uf': [curso['CLAVE'], grupo], 'Periodo': periodo})
 
-    # 随机打乱课程及其班级的顺序
+            # agregar profesor
+            # si el ultimo elemento de CLAVE no es B(bloque), agregar profesor en el archivo Profesores y Materias.csv
+            if curso['CLAVE'][-1] != 'B':
+                # open Profesores y Materias.csv
+                for clave, profesor in profesores_materias.items():
+                    if clave == curso['CLAVE']:
+                        print(profesor)
+                        # Add your code here to handle the 'profesor' variable
+
+            gen = {'UF': [curso['CLAVE'], grupo], 
+                   'Periodo': periodo}
+            cromosoma.append(gen)
+
+    # aleatorio de genes en el cromosoma
     random.shuffle(cromosoma)
 
     return cromosoma
 
-# 生成100个包含10个随机课程及其班级的种群
-# poblacion1 = [generar_cromosomas() for _ in range(100)]
+# generar poblacion
+#poblacion1 = [generar_cromosomas() for _ in range(100)]
 
-# 打印生成的种群
+# imprimir poblacion
 # for i, cromosoma in enumerate(poblacion1, 1):
 #     print(f"Cromosoma {i}:")
 #     for curso in cromosoma:
